7e442eb6e09dc6730dffc5737d6f598e38c29f9e
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2020-05-26 13:15:01 +0200

    feat: add support description

--- src/pages/Detail/components/SupportEdit.tsx
+++ src/pages/Detail/components/SupportEdit.tsx
@@ -1,5 +1,7 @@
+import Box from 'components/Box'
 import ExpansionPanel from 'components/ExpansionPanel'
 import SupportInput from 'components/forms/SupportInput'
+import { SubTitle } from 'components/Title'
 import { UnitSupport } from 'models/units'
 import { UserUnitSupport } from 'models/userBox'
 import React from 'react'
@@ -24,6 +26,23 @@ export default function SupportEdit ({
 
   return (
     <ExpansionPanel title="Support">
+      <Box my="2">
+        <SubTitle fontSize="1">
+          <strong>For: </strong>
+          {Characters}
+        </SubTitle>
+        <p>
+          <strong>max: </strong>
+          {description[description.length - 1]}
+        </p>
+        {lvl > 0 && lvl < 5 && (
+          <p>
+            <strong>Level {lvl}: </strong>
+            {description[lvl - 1]}
+          </p>
+        )}
+      </Box>
+
       <InputLabel value={lvl} max={5}>
         <SupportInput
           name="support"
@@ -35,13 +54,6 @@ export default function SupportEdit ({
           }
         />
       </InputLabel>
-
-      <p>{Characters}</p>
-      {description.map((desc, i) => (
-        <p key={i}>
-          {i}: {desc}
-        </p>
-      ))}
     </ExpansionPanel>
   )
 }
