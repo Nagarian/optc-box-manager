bfcbaf01c43e3baa96ec314264105e8e49ba5af2
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2020-06-01 14:01:05 +0200

    fix: issue with pagination

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function usePagination (itemCount: number, itemPerPage: number) {
   const [page, setPage] = useState<number>(1)
+
+  useEffect(() => {
+    const maxPage = Math.ceil(itemCount / itemPerPage) || 1
+    if (page > maxPage) setPage(maxPage)
+  }, [itemCount, itemPerPage, page])
 
   return {
     page: page,
     slice: [(page - 1) * itemPerPage, page * itemPerPage],
     paginationProps: {
       current: page,
-      maxPage: Math.ceil(itemCount / itemPerPage),
+      maxPage: Math.ceil(itemCount / itemPerPage) || 1,
     },
     reset: () => setPage(1),
     setPage,
