3e93537685c14023668fc027267624014f429db4
Nagarian <Nagarian@users.noreply.github.com>
2020-06-05 17:22:00 +0000

    deploy: 4503d6902a811c0cc625392023dae4b6fb07d52e

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.104d3a7cda3c80c93acb07ff991ee4df.js"
+  "./precache-manifest.dcabe3c2a89db22563e1d0772816ef0b.js"
 );
 
 self.addEventListener('message', (event) => {
