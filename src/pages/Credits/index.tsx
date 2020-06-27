4fc93b7dc2c3936fe32b8409d108ccf5a502d1ff
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2020-06-27 17:50:15 +0200

    docs: improve credits component

--- src/pages/Credits/index.tsx
+++ src/pages/Credits/index.tsx
@@ -2,6 +2,7 @@ import Button from 'components/Button'
 import Popup from 'components/Popup'
 import { SubTitle, Text } from 'components/Title'
 import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 export default function Credits () {
   const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -12,40 +13,34 @@ export default function Credits () {
 
   return (
     <Popup onClose={() => setIsOpen(false)} title="Credits">
-      {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
-      <Text m="1">
-        Made with 💗 and ☕ by{' '}
-        <a
-          href="https://www.reddit.com/user/nagarian_r"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          u/nagarian_r
-        </a>
-      </Text>
-      <Text m="1">
-        Please report bugs and suggestions into{' '}
-        <a
-          href="https://github.com/Nagarian/optc-box-manager/issues"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </Text>
-      <Text m="1">
-        This site does not own any of the images. All images are owned by
-        Eiichiro Oda/Shueisha, Toei Animation, and Bandai Namco.
-      </Text>
-      <SubTitle>IMPORTANT</SubTitle>
-      <Text m="1">
-        Your box is saved <b>ONLY LOCALLY</b>
-      </Text>
-      <Text m="1">Clearing your browser cache will delete your box !</Text>
-      <Text m="1">
-        If you want to view your box in a difference device, you should export
-        and import it manually
-      </Text>
+      <ReactMarkdown
+        source={creditText}
+        renderers={{
+          heading: p => <SubTitle {...p} m="2" />,
+          paragraph: p => <Text {...p} m="1" />,
+          // eslint-disable-next-line jsx-a11y/anchor-has-content
+          link: p => <a {...p} target="_blank" rel="noopener noreferrer" />,
+        }}
+      />
     </Popup>
   )
 }
+
+const creditText = `
+Made with 💗 and ☕ by [u/nagarian_r](https://www.reddit.com/user/nagarian_r)
+
+Please report bugs and suggestions into [Github](https://github.com/Nagarian/optc-box-manager/issues)
+
+All the information originates from [OPTC-DB](https://optc-db.github.io/)
+
+This site does not own any of the images. All images are owned by Eiichiro Oda/Shueisha, Toei Animation, and Bandai Namco.
+
+# IMPORTANT
+
+Your box is saved **ONLY LOCALLY**
+
+Clearing your browser cache will delete your box !
+
+If you want to view your box in a different device, you should export
+and import it manually
+`
