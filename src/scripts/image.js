aa192c4d7676bec3b894d6a385048a12ad096820
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2022-03-31 17:26:35 +0200

    chore(db): fix images path

--- src/scripts/image.js
+++ src/scripts/image.js
@@ -15,25 +15,11 @@ const getImage = (
   try {
     const imagePath = func(id)
     return imagePath
-      ?.replace('../res', 'https://optc-db.github.io/res/')
+      ?.replace('/api/images', 'https://optc-db.github.io/api/images')
       .replace('http:', 'https:')
   } catch (error) {
     console.trace('Invalid unit :', id)
     return 'https://onepiece-treasurecruise.com/wp-content/themes/onepiece-treasurecruise/images/noimage.png'
-  }
-}
-
-async function downloadCharacters (
-  /** @type { import('models/old-units').ExtendedUnit[] } */ DBUnits,
-) {
-  const { createWriteStream } = require('fs')
-  const http = require('https')
-
-  for (const unit of DBUnits) {
-    http.get(unit.images.thumbnail, res => {
-      res.pipe(createWriteStream(`characters_images/${unit.id}.png`))
-    })
-    await sleep(1000)
   }
 }
 
@@ -46,5 +32,4 @@ function sleep (/** @type number */ ms) {
 module.exports = {
   getUnitThumbnail: getImage(Utils.getThumbnailUrl),
   getUnitFullPicture: getImage(Utils.getBigThumbnailUrl),
-  downloadCharacters,
 }
