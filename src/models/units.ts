4271719d5f12b70509d616a712ee6703d988f828
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2023-05-15 13:12:30 +0200

    feat: add support for "Triple Special Activation" potential

--- src/models/units.ts
+++ src/models/units.ts
@@ -106,6 +106,7 @@ export const Potentials = [
   'Last Tap',
   'Super Tandem',
   'Last Tap / Super Tandem',
+  'Triple Special Activation',
 ] as const
 
 export type PotentialKey = (typeof Potentials)[number]
